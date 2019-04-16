5b6043a009852869246f5efdec952adb7147d9e1
Aki Kaivola <aki.kaivola@gmail.com>
2019-04-16 22:06:05 +0300

    Update project dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.5.0"]
                  [yogthos/config "0.8"]
-                 [ring "1.4.0"]]
+                 [funcool/cuerdas "2.0.5"]
+                 [ring "1.4.0"]
+                 [com.taoensso/timbre "4.10.0"]]
 
   :plugins [[lein-less "1.7.5"]]
 
@@ -17,6 +19,11 @@
   :less {:source-paths ["resources/less"]
          :target-path  "resources/public/css/compiled"}
 
+  :aliases {"dev" ["with-profile" "dev"
+                   ["pdo"
+                    ["run"]
+                    ["less" "auto"]]]}
+
   :profiles
   {:dev
    {:dependencies [[org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]
@@ -25,12 +32,19 @@
                    [day8.re-frame/re-frame-10x "0.3.7-react16"]
                    [day8.re-frame/tracing "0.5.1"]]
 
-    :plugins      [[refactor-nrepl "2.4.1-SNAPSHOT"]
-                   [cider/cider-nrepl "0.22.0-SNAPSHOT"]]}
-   :prod { :dependencies []}
-   :uberjar {:dependencies []
-             :omit-source  true
-             :main         multimap.server
-             :aot          [multimap.server]
-             :uberjar-name "multimap.jar"
-             :prep-tasks   ["compile" ["less" "once"]]}})
+    :plugins [[lein-pdo "0.1.1"]
+              [refactor-nrepl "2.4.1-SNAPSHOT"]
+              [cider/cider-nrepl "0.22.0-SNAPSHOT"]]}
+   :prod    {:dependencies []}
+   :uberjar {:dependencies   []
+             :omit-source    true
+             :aot            :all
+             :resource-paths ["resources"]}}
+
+  :main         multimap.server
+  :aot          [multimap.server]
+
+  :uberjar {:main         multimap.server
+            :aot          [multimap.server]
+            :uberjar-name "multimap.jar"
+            :prep-tasks   ["compile" ["less" "once"]]})
