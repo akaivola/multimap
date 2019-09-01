0a24a23afcb199b70a1c2c2876e1125c38fe1f51
Aki Kaivola <aki.kaivola@gmail.com>
2019-09-01 15:41:13 +0300

    Add mapbox-gl default terrain map

--- src/cljs/multimap/map.cljs
+++ src/cljs/multimap/map.cljs
@@ -1,13 +1,59 @@
 (ns multimap.map
   (:require
+   [crate.core :as crate]
    [mapbox-gl]
+   [multimap.config :as config]
+   [oops.core :refer [oget oset!]]
    [re-frame.core :as r]
    [reagent.core :as re]
-   [multimap.config :as config]
-   [oops.core :refer [oget]]
-   [taoensso.timbre :refer-macros [spy debug]]))
+   [taoensso.timbre :refer-macros [spy debug]]
+   ))
 
 (def token config/mapbox-public-token)
 
+(def Marker (oget mapbox-gl "Marker"))
+
+(def map-ref (atom nil))
+
+(def options
+  {:container "map"
+   :style     "mapbox://styles/mapbox/outdoors-v9"
+   :center    [24.94311914138914
+               60.32116290719057]
+   :zoom      13})
+
+(defn gl-map []
+  (re/create-class
+    {:component-did-mount
+     (fn []
+       (oset!
+         mapbox-gl
+         "accessToken"
+         token)
+
+       (let [Map   (oget mapbox-gl "Map")
+             glmap (Map.
+                     (clj->js
+                       options))]
+         (.on
+           glmap
+           "load"
+           (fn []
+             (reset! map-ref glmap)
+             (oset! js/window "map" glmap)
+             #_(doto glmap
+                    (.addLayer (clj->js mapant)))
+
+             (.on glmap "click"
+                  (fn [e]
+                    (debug [(oget e "lngLat" "lng") (oget e "lngLat" "lat")])))))
+))
+     :component-will-unmount
+     (fn []
+       )
+     :reagent-render
+     (fn []
+       [:div#map])}))
+
 (defn map-panel []
-  [:div "map"])
+  [gl-map])
