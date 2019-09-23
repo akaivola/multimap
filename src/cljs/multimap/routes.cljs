e866b83f3febeb34c35f486430ea6fcb29cc90d2
Aki Kaivola <aki.kaivola@gmail.com>
2019-09-23 11:13:20 +0300

    Remove unused boilerplate

--- src/cljs/multimap/routes.cljs
+++ src/cljs/multimap/routes.cljs
@@ -23,30 +23,7 @@
   ;; --------------------
   ;; define routes here
   (defroute "/" []
-    (re-frame/dispatch [::events/set-active-panel :home-panel])
-    (re-frame/dispatch [::events/set-re-pressed-example nil])
-    (re-frame/dispatch
-     [::rp/set-keydown-rules
-      {:event-keys [[[::events/set-re-pressed-example "Hello, world!"]
-                     [{:which 72} ;; h
-                      {:which 69} ;; e
-                      {:which 76} ;; l
-                      {:which 76} ;; l
-                      {:which 79} ;; o
-                      ]]]
-
-       :clear-keys
-       [[{:which 27} ;; escape
-         ]]}])
-    )
-
-  (defroute "/about" []
-    (re-frame/dispatch [::events/set-active-panel :about-panel]))
-
-  (defroute "/map" []
-    (re-frame/dispatch [::events/set-active-panel :map-panel])
-    )
-
+    (re-frame/dispatch [::events/set-active-panel :map-panel]))
 
   ;; --------------------
   (hook-browser-navigation!))
