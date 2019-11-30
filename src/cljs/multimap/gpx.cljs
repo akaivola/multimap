60add7d92cd862d15f847e7f977dc0ddc24d23f8
Aki Kaivola <aki.kaivola@gmail.com>
2019-11-30 21:34:39 +0200

    Add GPX -> toGeoJSON

---
+++ src/cljs/multimap/gpx.cljs
@@ -0,0 +1,9 @@
+(ns multimap.gpx)
+
+(defn domparse [string]
+  (.parseFromString (js/DomParser.)  string "text/xml"))
+
+(def gpx (.-gpx js/toGeoJSON))
+
+(defn to-geo-json [string]
+  (-> string domparse gpx))
