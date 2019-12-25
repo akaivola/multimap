9884b99cbf1fbad81f05ca2baedcf6f9fd17a73c
Aki Kaivola <aki.kaivola@gmail.com>
2019-12-25 21:14:47 +0200

    Update re-frame db with current description of data

--- src/cljs/multimap/db.cljs
+++ src/cljs/multimap/db.cljs
@@ -1,4 +1,14 @@
 (ns multimap.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:map
+   {:gps
+    {:id nil
+     :marker nil
+     :position
+     {:latitude nil
+      :longitude nil
+      :accuracy nil
+      :heading nil
+      :type nil
+      :speed nil}}}})
